c41347a1611b13fa810add0f9419ce25b8a3e663
Veronika Nevařilová <xnevar00@stud.fit.vutbr.cz>
2023-10-03 12:53:26 +0200

    Minor fixes

--- backend/IISProject/IISProject.Api.DAL/Entities/KpiEntity.cs
+++ backend/IISProject/IISProject.Api.DAL/Entities/KpiEntity.cs
@@ -4,7 +4,7 @@ public record KpiEntity : IEntity
 {
     public required Guid Id { get; set; }
     public required string Function { get; set; }
-    public string? Error { get; set; }
+    public bool? Error { get; set; }
 
     public UserEntity? Creator { get; init; }
     public DeviceEntity? Device { get; init; }
