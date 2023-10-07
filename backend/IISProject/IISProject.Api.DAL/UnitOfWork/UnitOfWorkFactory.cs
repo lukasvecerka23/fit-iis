d57749e25e7fc390a9e9d651ca3a41066a7c25f2
Lukas Vecerka <vecerkalukas18@gmail.com>
2023-10-07 10:38:51 +0200

    implemented uow factory

--- backend/IISProject/IISProject.Api.DAL/UnitOfWork/UnitOfWorkFactory.cs
+++ backend/IISProject/IISProject.Api.DAL/UnitOfWork/UnitOfWorkFactory.cs
@@ -1,6 +1,21 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
 namespace IISProject.Api.DAL.UnitOfWork;
 
-public class UnitOfWorkFactory
+public class UnitOfWorkFactory: IUnitOfWorkFactory
 {
-    // TODO: implement after the db context will be finished
+    private readonly IDbContextFactory<IISProjectDbContext> _dbContextFactory;
+    private readonly IMapper _mapper;
+
+    public UnitOfWorkFactory(IDbContextFactory<IISProjectDbContext> dbContextFactory, IMapper mapper)
+    {
+        _dbContextFactory = dbContextFactory;
+        _mapper = mapper;
+    }
+
+    public IUnitOfWork Create()
+    {
+        return new UnitOfWork(_dbContextFactory.CreateDbContext(), _mapper);
+    }
 }
