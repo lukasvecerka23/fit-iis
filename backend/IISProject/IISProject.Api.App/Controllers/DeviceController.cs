b192074f200832e8b026618d606f626ff4c1b441
Lukas Vecerka <vecerkalukas18@gmail.com>
2023-10-08 00:22:02 +0200

    create first controller and get API running

--- backend/IISProject/IISProject.Api.App/Controllers/DeviceController.cs
+++ backend/IISProject/IISProject.Api.App/Controllers/DeviceController.cs
@@ -1,32 +1,25 @@
+using IISProject.Api.BL.Facades;
+using IISProject.Api.BL.Models.Device;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IISProject.Controllers;
 
 [ApiController]
-[Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+[Route("api/devices")]
+public class DeviceController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+    private readonly ILogger<DeviceController> _logger;
+    private readonly DeviceFacade _deviceFacade;
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public DeviceController(ILogger<DeviceController> logger, DeviceFacade deviceFacade)
     {
         _logger = logger;
+        _deviceFacade = deviceFacade;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public async Task<IEnumerable<DeviceListModel>> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+        return await _deviceFacade.GetAllAsync();
     }
 }
