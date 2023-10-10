67bb75cef0991b63b1499563f6b0eb7e73ad5926
Lukas Vecerka <vecerkalukas18@gmail.com>
2023-10-10 12:05:41 +0200

    create device controller

--- backend/IISProject/IISProject.Api.App/Controllers/DeviceController.cs
+++ backend/IISProject/IISProject.Api.App/Controllers/DeviceController.cs
@@ -17,9 +17,34 @@ public class DeviceController : ControllerBase
         _deviceFacade = deviceFacade;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public async Task<IEnumerable<DeviceListModel>> Get()
+    [HttpGet]
+    public async Task<IEnumerable<DeviceListModel>> GetDevices()
     {
         return await _deviceFacade.GetAllAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<DeviceDetailModel?> GetDeviceById(Guid id)
+    {
+        return await _deviceFacade.GetByIdAsync(id);
+    }
+
+    [HttpPost]
+    public async Task<DeviceDetailModel> CreateDevice(DeviceDetailModel device)
+    {
+        return await _deviceFacade.SaveAsync(device);
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<DeviceDetailModel> UpdateDevice(Guid id, DeviceDetailModel device)
+    {
+        device.Id = id;
+        return await _deviceFacade.SaveAsync(device);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task DeleteDevice(Guid id)
+    {
+        await _deviceFacade.DeleteAsync(id);
     }
 }
