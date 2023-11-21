bb06ed2893d871aae8a5b8c24936c82bba8fe173
Lukas Vecerka <vecerkalukas18@gmail.com>
2023-11-21 14:29:20 +0100

    add router

---
+++ client/src/lib/Router.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { Router, Route } from 'svelte-routing';
+    import Counter from './Counter.svelte';
+
+</script>
+
+<Router>
+    <Route path="/" component={Counter}/>
+</Router>
