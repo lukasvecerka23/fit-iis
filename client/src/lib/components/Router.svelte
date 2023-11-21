f40c88515c3231dd5447985e05edde15be92a59d
Veronika Nevařilová <xnevar00@stud.fit.vutbr.cz>
2023-11-21 16:43:35 +0100

    made side and top bar

---
+++ client/src/lib/components/Router.svelte
@@ -0,0 +1,11 @@
+<!-- Router.svelte -->
+<script>
+    import { Router, Route, Link } from 'svelte-routing'
+    import HomePage from '../pages/home-page/HomePage.svelte';
+    import Systems from '../pages/systems/Systems.svelte';
+</script>
+  
+<Router>
+    <Route path="home" component={HomePage} />
+    <Route path="systems" component={Systems} />
+</Router>
