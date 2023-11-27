bd732ffb0923e1c3c7bd009bd3fa8ff336651a0e
Lukas Vecerka <vecerkalukas18@gmail.com>
2023-11-28 00:39:11 +0100

    conflicts

--- backend/IISProject/IISProject.Api.App/Program.cs
+++ backend/IISProject/IISProject.Api.App/Program.cs
@@ -1,13 +1,64 @@
+using System.Text;
+using AutoMapper;
+using FluentValidation.AspNetCore;
+using IISProject.Api.BL.Installers;
+using IISProject.Api.BL.Models.Responses;
+using IISProject.Api.Common.Extensions;
+using IISProject.Api.DAL.Entities;
+using IISProject.Api.DAL.Installers;
+using IISProject.Api.DAL.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
+ConfigureDependencies(builder.Services, builder.Configuration);
+ConfigureAutoMapper(builder.Services);
+ConfigureCors(builder.Services, builder.Configuration);
+ConfigureControllers(builder.Services);
+builder.Services.AddFluentValidationAutoValidation();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            if (context.Request.Cookies.ContainsKey("jwt"))
+            {
+                context.Token = context.Request.Cookies["jwt"];
+            }
+            return Task.CompletedTask;
+        }
+    };
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration["JwtConfig:Issuer"],
+        ValidAudience = builder.Configuration["JwtConfig:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtConfig:Secret"]!)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
+
+builder.Services.AddAuthorization();
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
+
+ValidateAutoMapperConfiguration(app.Services);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,10 +67,74 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Migrate database
+using var scope = app.Services.CreateScope();
+if (app.Environment.IsDevelopment())
+{
+    scope.ServiceProvider.GetRequiredService<IDbMigrator>().Migrate();
+}
+
+app.UseCors();
+
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+void ConfigureCors(IServiceCollection serviceCollection, IConfiguration configuration)
+{
+    serviceCollection.AddCors(options =>
+    {
+        options.AddDefaultPolicy(o =>
+            o.WithOrigins(configuration["Cors:AllowedOrigins"] ?? "")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials());
+    });
+}
+
+void ConfigureDependencies(IServiceCollection serviceCollection, IConfiguration configuration)
+{
+    var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new ArgumentException("The connection string is missing");
+    serviceCollection.AddInstaller<ApiDALInstaller>(connectionString);
+    serviceCollection.AddInstaller<ApiBLInstaller>();
+}
+
+void ConfigureAutoMapper(IServiceCollection serviceCollection)
+{
+    serviceCollection.AddAutoMapper(typeof(IEntity), typeof(ApiBLInstaller));
+}
+
+void ValidateAutoMapperConfiguration(IServiceProvider serviceProvider)
+{
+    var mapper = serviceProvider.GetRequiredService<IMapper>();
+    mapper.ConfigurationProvider.AssertConfigurationIsValid();
+}
+
+void ConfigureControllers(IServiceCollection serviceCollection)
+{
+    // Configure better error messages for invalid input
+    serviceCollection.AddControllers()
+        .ConfigureApiBehaviorOptions(options =>
+        {
+            options.InvalidModelStateResponseFactory = context =>
+            {
+                var errors = context.ModelState
+                    .Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage)
+                    .ToList();
+
+                var errorResponse = new BadRequestModel
+                {
+                    Errors = errors
+                };
+
+                return new BadRequestObjectResult(errorResponse);
+            };
+        });
+}
